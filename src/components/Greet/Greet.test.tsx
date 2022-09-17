9671eb1f44f1281a23c50fb9a3fa49f399921795
rgsk <guptarahul70322@gmail.com>
2022-09-18 00:10:52 +0530

    cleanup

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -2,15 +2,13 @@ import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
 describe('Greet', () => {
-  // test.only
-  fit('renders correctly', () => {
+  test('renders correctly', () => {
     render(<Greet />);
     const textElement = screen.getByText('Hello');
     expect(textElement).toBeInTheDocument();
   });
 
-  // test.skip
-  xit('renders with a name', () => {
+  test('renders with a name', () => {
     render(<Greet name="Rahul" />);
     const textElement = screen.getByText('Hello Rahul');
     expect(textElement).toBeInTheDocument();
