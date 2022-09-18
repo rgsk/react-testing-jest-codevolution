1e22f50201a2e06c7a237db546fe022ff6d689f6
rgsk <guptarahul70322@gmail.com>
2022-09-18 17:58:59 +0530

    assertions, jest-dom and matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// setupTests file will be automatically ran, before jest runs a test
+// this is the reason import '@testing-library/jest-dom'; statement will run
+// and methods like expect(textElement).toBeInTheDocument() will be available to us
