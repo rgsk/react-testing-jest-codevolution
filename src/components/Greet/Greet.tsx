0df9d040bff93ec800782621328735c9718a6cae
rgsk <guptarahul70322@gmail.com>
2022-09-18 17:05:49 +0530

    include and exclude files from coverage report

--- src/components/Greet/Greet.tsx
+++ src/components/Greet/Greet.tsx
@@ -1,6 +1,5 @@
-interface IGreetProps {
-  name?: string;
-}
+import { IGreetProps } from './greet.types';
+
 const Greet: React.FC<IGreetProps> = ({ name }) => {
   return <div>Hello {name}</div>;
 };
