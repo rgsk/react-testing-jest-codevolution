4eb82b38694504d0154583420072cc33e368cf11
rgsk <guptarahul70322@gmail.com>
2022-09-19 23:06:02 +0530

    fix

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -30,7 +30,7 @@ export const Application = () => {
           <textarea id="bio" />
         </div>
         <div>
-          <label htmlFor="job-location">Name</label>
+          <label htmlFor="job-location">Job location</label>
           <select id="job-location">
             <option value="">Select a country</option>
             <option value="US">United States</option>
