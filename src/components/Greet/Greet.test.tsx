a9e9cc6fb5edeafa130ebe0d4453db98f9da26c3
rgsk <guptarahul70322@gmail.com>
2022-09-19 22:27:37 +0530

    getByRole

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -4,6 +4,10 @@ import Greet from './Greet';
 describe('Greet', () => {
   test('renders correctly', () => {
     render(<Greet />);
+    /*
+    getBy.. class of queries return the matching node for a query, and throw a 
+    descriptive error if no elements match or if more than one match is found
+    */
     const textElement = screen.getByText('Hello Guest');
     expect(textElement).toBeInTheDocument();
   });
